e343ffba2dec121f7e87708bb4352c293c8fffa1
Gabriel Botan <gabrielficciobotan@gmail.com>
2025-02-17 18:50:17 -0300

    Update Microsoft.PowerShell_profile.ps1

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,21 +1,14 @@
-# Oh My Posh
-oh-my-posh init pwsh --config 'C:\Users\gabri\AppData\Local\Programs\oh-my-posh\themes\the-unnamed.omp.json' | Invoke-Expression
-
 cls
 
+# Oh My Posh
+# oh-my-posh init pwsh --config 'C:\Program Files (x86)\oh-my-posh\themes\kali.omp.json' | Invoke-Expression
+
 # Starship
-# starship init powershell | Out-String | Invoke-Expression
-# $ENV:STARSHIP_CONFIG = "C:\Users\gabri\.config\starship.toml"
+ starship init powershell | Out-String | Invoke-Expression
+ $ENV:STARSHIP_CONFIG = "C:\Users\gabri\.config\starship.toml"
 
 # colors
 $env:LS_COLORS = "di=93"
-
-# Posh-git function
-function LoadPoshGit {
-    if (-not (Get-Module -Name posh-git)) {
-        Import-Module posh-git
-    }
-}
 
 # Chocolatey Profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
@@ -23,18 +16,22 @@ if (Test-Path($ChocolateyProfile)) {
     Import-Module "$ChocolateyProfile"
 }
 
-# Git module (on demand)
-Set-Alias poshgit LoadPoshGit
-
 # Use lsd
 Set-Alias ls lsd
 
 # Aliases
+function poff { shutdown /p }
+
 function init { git init }
 
 function gcl { git clone $args }
 
 function gst { git status }
+
+function wip {
+    git add .
+    git commit -m "wip"
+}
 
 function ga { git add . }
 
@@ -45,3 +42,5 @@ function gsh { git push }
 function rd { param([string]$path) Remove-Item -Path $path -Recurse -Force }
 
 function mfs { php artisan migrate:fresh --seed }
+
+function as { php artisan serve }
